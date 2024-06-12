b65a7136acc3a5eb6b5704686519dc672e96f50f
Abhishek047 <abhi.shukla0047@gmail.com>
2024-06-12 10:41:21 +0530

    Fixed the routers and db connection

--- src/server.js
+++ src/server.js
@@ -8,35 +8,33 @@ const PORT = process.env.PORT || 8080;
 
 // mongo connection
 const MONGO_URI =
-  process.env.IS_PRODUCTION || process.env.IS_QA || process.env.IS_DEV
-    ? `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_CLUSTER}.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`
-    : `mongodb://localhost:27017/spark`;
+    process.env.IS_PRODUCTION || process.env.IS_QA || process.env.IS_DEV
+        ? `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_CLUSTER}.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`
+        : `mongodb://localhost:27017/spark`;
 
 const connectDatabase = async () => {
-  await mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+    await mongoose.connect(MONGO_URI);
 };
 
 try {
-  connectDatabase();
-  console.log("Database connected...");
+    connectDatabase();
+    console.log("Database connected...");
 } catch (error) {
-  console.log("Mongo connection error");
+    console.log("Mongo connection error");
 }
 
 app.use(express.json());
 
 routers.forEach((router) => {
-  app.use(`/api${router.prefix}`, createRouter(router.routes, []));
+    console.log(router);
+    app.use(`/api${router.prefix}`, createRouter(router.routes, []));
 });
 
 app.listen(PORT, () => {
-  console.log(`App is listening to port ${PORT}`);
+    console.log(`App is listening to port ${PORT}`);
 });
 
 process.on("SIGINT", () => {
-  console.log("Stopping server...");
-  process.exit();
+    console.log("Stopping server...");
+    process.exit();
 });
--- src/tasks/getTasks.js
+++ src/tasks/getTasks.js
@@ -1,9 +1,10 @@
 export const getTasks = {
-  path: "/",
-  method: "get",
-  handler: (req, res) => {
-    return res.status(200).json({
-      success: true,
-    });
-  },
+    path: "/",
+    method: "get",
+    handler: (req, res) => {
+        console.log("route-hit");
+        return res.status(200).json({
+            success: true,
+        });
+    },
 };
